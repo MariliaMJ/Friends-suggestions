8f59a8a66b1610a1e9d676f370248ac2f080d76d
MariliaMJ <mmjanizelli@gmail.com>
2017-06-08 00:22:03 -0300

    alterações de viw model setting

--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -5,6 +5,5 @@ from neo4django.db import models
 
 class Person(models.NodeModel):
     name = models.StringProperty()
-    age = models.IntegerProperty()
-
+    #age = models.IntegerProperty()
     friends = models.Relationship('self',rel_type='friends_with')
--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from quickstart.serializers import UserSerializer, GroupSerializer
-
+from django.contrib.auth.models import Person
+from rest_framework import viewsetstes
 
 class UserViewSet(viewsets.ModelViewSet):
     """
