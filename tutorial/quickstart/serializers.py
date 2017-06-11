c8db0d47e32dae9f0e1ca12b1c208c37784bb023
MariliaMJ <mmjanizelli@gmail.com>
2017-06-11 16:42:51 -0300

    db server

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'neo4django.graph_auth',
+    'south',
 ]
 
 REST_FRAMEWORK = {
